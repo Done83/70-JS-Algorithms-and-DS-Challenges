685c0e5bd79570d94677f39570654c449b606a12
Done83 <nedomehan@gmail.com>
2024-11-12 08:15:50 +0100

    Array Intersection challenge completed

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,12 @@
-function arrayIntersection() {}
+function arrayIntersection(arrOne, arrTwo) {
+    const intercestion = [];
+    for (let i = 0;  i < arrOne.length; i++) {
+        if (arrOne.includes(arrTwo[i])) {
+            intercestion.push(arrTwo[i])
+        }
+    }
+
+    return intercestion
+}
 
 module.exports = arrayIntersection;
