a2587a5d5eb7903367553a985a1e293b8405dd95
Done83 <nedomehan@gmail.com>
2024-11-13 11:01:57 +0100

    Find missing number challenge completed

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,12 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+    let n = arr.length + 1;
+    let expectedSum = n * (n + 1) / 2;
+    let actualSum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        actualSum += arr[i];
+    }
+
+    return expectedSum - actualSum;
+}
 
 module.exports = findMissingNumber;
