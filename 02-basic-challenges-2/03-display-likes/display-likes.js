501adee0634aed3823799d88e460b4b1f43883b8
Done83 <nedomehan@gmail.com>
2024-11-13 10:48:08 +0100

    Display Likes challenge completed

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,16 @@
-function displayLikes() {}
+function displayLikes(array) {
+    let length = array.length;
+    if (length === 0) {
+        return `no one likes this`
+    } else if (length === 1) {
+        return `${array[0]} likes this`
+    } else if (length === 2) {
+        return `${array[0]} and ${array[1]} like this`
+    } else if (length === 3) {
+        return `${array[0]}, ${array[1]} and ${array[2]} like this`
+    } else {
+        return `${array[0]}, ${array[1]} and ${length - 2} others like this`
+    }
+}
 
 module.exports = displayLikes;
